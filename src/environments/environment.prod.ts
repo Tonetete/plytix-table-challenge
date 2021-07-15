bfa716ae8604de8a8f2f071e96feffab3d9b8116
Antonio Rivas <toneterivas@gmail.com>
2021-07-15 21:02:04 +0200

    set cors anywhere for endpoint in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  base_api_url: 'http://34.241.217.201/users_ARF/',
+  base_api_url:
+    'https://cors-anywhere.herokuapp.com/http://34.241.217.201/users_ARF/',
 };
